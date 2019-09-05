79480d92fca1c836630c2906cda509559a44b6a7
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-09-05 16:28:27 +0530

    Channges in remapping #PUNCT.. big labels into characters

--- utf8_to_wx.sh
+++ utf8_to_wx.sh
@@ -3,8 +3,9 @@
 ./utf8_to_wx.out < temp2 > temp
 ./replace_nukta.out < temp > temp1 
 ./replace_a_.out <temp1 >temp3
+#./remove_deli.out < temp3 >$2
 ./remove_deli.out < temp3 >temp4
-./label_remove.out <temp4
+./label_remove.out <temp4 > $2
 
-rm temp0 temp temp1 temp2 temp3 temp4
+#rm temp0 temp temp1 temp2 temp3 temp4
 
