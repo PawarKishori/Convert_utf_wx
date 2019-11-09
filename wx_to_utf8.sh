47e92258bb99f77720478537715f0fed5b536308
RajRajeshwari <36527493+RajRajeshwari@users.noreply.github.com>
2019-11-09 14:50:32 +0530

    error fixed

--- wx_to_utf8.sh
+++ wx_to_utf8.sh
@@ -1,4 +1,4 @@
-wx_to_utf8.out  > temp
-wx_replace_a_.out < temp >temp1
-remove_engtag.out <temp1
+./wx_to_utf8.out <$1  > temp
+./wx_replace_a_.out < temp >temp1
+./remove_engtag.out <temp1 >$2
 rm temp temp1
